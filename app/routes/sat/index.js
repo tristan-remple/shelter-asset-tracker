ec31909844805b0f9ffa675354da3a042ba70af6
KMicheleMoore <kmichele0831@gmail.com>
2024-04-09 02:59:06 -0300

    model and routing update

--- app/routes/sat/index.js
+++ app/routes/sat/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 /*SAT Routes*/
+const commentsRouter = require('./comments');
 const facilitiesRouter = require('./facilities');
 const itemsRouter = require('./items');
 const templateRouter = require('./templates');
 const unitsRouter = require('./units');
 const usersRouter = require('./users');
 
+router.use('/comments', commentsRouter);
 router.use('/facilities', facilitiesRouter);
-//router.use('/items', itemsRouter);
+router.use('/items', itemsRouter);
 router.use('/templates', templateRouter);
 router.use('/units', unitsRouter);
 router.use('/users', usersRouter);
