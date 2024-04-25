23c8ecf702d0304063fcdc6ad533878e4a094b1a
tristan-remple <tristan.remple@outlook.com>
2024-04-25 19:35:10 -0300

    dashboard filtering coming along well

--- client/src/Admin/Dashboard.jsx
+++ client/src/Admin/Dashboard.jsx
@@ -27,9 +27,14 @@ const Dashboard = () => {
     const { status } = useContext(statusContext)
     const [ err, setErr ] = useState("loading")
 
-    let viewOptions = []
+    // view options: list of location names to filter by dropdown
+    // view: currently selected location
+    const [ viewOptions, setViewOptions ] = useState([])
+    const [ view, setView ] = useState("All Locations")
+    
     // fetch unit data from the api
     const [ response, setResponse ] = useState()
+
     useEffect(() => {
         (async()=>{
             await apiService.globalReport(function(data){
@@ -37,59 +42,100 @@ const Dashboard = () => {
                     setErr(data.error)
                 } else {
                     setResponse(data)
-                    console.log(data)
-                    viewOptions = data.map(loc => loc.name)
-                    viewOptions.unshift("All Locations")
+                    const viewOpt = data.map(loc => loc.facility)
+                    viewOpt.unshift("All Locations")
+                    setViewOptions(viewOpt)
                     setErr(null)
                 }
             })
         })()
     }, [])
 
-    const [ view, setView ] = useState("All Locations")
-
+    // state to hold the information about the currently selected view
     const [ discardItems, setDiscardItems ] = useState([])
     const [ filteredItems, setFilteredItems ] = useState([])
     const [ totalValue, setTotalValue ] = useState(0)
     const [ itemCount, setItemCount ] = useState([])
 
     useEffect(() => {
+
+        // if the api call has been completed successfully and the view is set to all
         if (view === "All Locations" && response?.length > 0) {
+
+            // initialize arrays and count
             let newItemList = []
             let newItemCount = []
             let newTotalValue = 0
+
+            // loop through the locations
             response.forEach(loc => {
-                loc.units.reduce((newItemList, unit) => {
+
+                // add the items from each unit to the total
+                newItemList = loc.units.reduce((newItemList, unit) => {
                     newItemList.push(...unit.items)
                     return newItemList
                 }, newItemList)
                 
-                loc.itemCount.reduce((newItemCount, cat) => {
+                // tally up the category counts
+                newItemCount = loc.itemCount.reduce((newItemCount, cat) => {
+
+                    // check if the tally for this category is initialized
+                    // add to it if it exists; pass forward existing categories
                     const matchingCat = newItemCount.filter(newCat => newCat.id === cat.id)[0]
                     if (matchingCat) {
-                        console.log("match")
                         newItemCount = newItemCount.map(newCat => {
                             if (newCat.id === cat.id) {
-                                newCat.count += cat.count
-                                console.log(newCat.count)
+                                return {
+                                    id: newCat.id,
+                                    name: newCat.name,
+                                    count: newCat.count + cat.count
+                                }
+                            } else {
+                                return newCat
                             }
-                            return newCat
                         })
+                    
+                    // if the category is not already in the counts, add it
                     } else {
                         newItemCount.push(cat)
                     }
                     return newItemCount
                 }, newItemCount)
 
+                // total up the value
                 newTotalValue += loc.totalValue
             })
+
+            // set all the totals to state
             setDiscardItems(newItemList)
             setFilteredItems(newItemList.filter(item => {
                 return item.toDiscard
             }))
             setItemCount(newItemCount)
             setTotalValue(newTotalValue)
+
+        // if the api call has succeeded and one location has been selected
+        } else if (response?.length > 0) {
+
+            // filter down to that location only
+            const location = response.filter(loc => {
+                return loc.facility === view
+            })[0]
+
+            // list the items in all of its units
+            const items = location.units.reduce((itemList, unit) => {
+                itemList.push(...unit.items)
+                return itemList
+            }, [])
+            
+            // set its values to state
+            setDiscardItems(items)
+            setFilteredItems(items)
+            setTotalValue(location.totalValue)
+            setItemCount(location.itemCount)
         }
+
+    // do this whole process when the api call completes and when the view changes
     }, [ response, view ])
 
     // table rows for the upper table: list of categories and number of items in each
@@ -117,9 +163,10 @@ const Dashboard = () => {
         discard: true
     })
 
+    // --CHANGES NEEDED HERE
     // when filters are updated, update the items listed
     useEffect(() => {
-        const newFilters = discardItems.filter(item => {
+        const newFilters = discardItems.filter(item => { // discardItems -> filterItems when EoL is figured out
             return (
                 new Date(item.eol) > new Date(filters.startDate) &&
                 new Date(item.eol) < new Date(filters.endDate) &&
@@ -131,11 +178,11 @@ const Dashboard = () => {
     }, [ filters ])
 
     // render the filtered items as table rows for the lower table
-    const displayItems = filteredItems.map(item => {
+    const displayItems = discardItems.map(item => {
         return (
             <tr key={ item.id } >
                 <td>{ item.name }</td>
-                {/* <td>{ capitalize(item.template.name) }</td> */}<td></td>
+                <td>{ capitalize(item.template) }</td>
                 <td><Button text="Details" linkTo={ `/item/${ item.id }` } type="small" /></td>
                 <td>{ adminDate(item.eol) }</td>
             </tr>
