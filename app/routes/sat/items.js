9fef9140aad45b6aff1144f93f1b7a1197fcda56
kmich <kmichele0831@gmail.com>
2024-04-28 23:10:59 -0300

    implemented new auth in item routes

--- app/routes/sat/items.js
+++ app/routes/sat/items.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const admin = require('../../middleware/admin');
+const auth = require('../../middleware/auth');
 
 // Import item controller
 const itemController = require('../../controllers/itemController');
@@ -8,11 +9,11 @@ const itemController = require('../../controllers/itemController');
 // Define routes for handling item operations
 router.route('/')
     .get(admin, itemController.getAllItems)
-    .post(itemController.createNewItem);
+    .post(itemController.checkFacility, auth, itemController.createNewItem);
 
 router.route('/:id')
-    .get(itemController.getItemById)
-    .put(itemController.updateItem)
-    .delete(itemController.deleteItem);
+    .get(itemController.getItemById, auth, itemController.sendItem)
+    .put(itemController.getItemById, auth, itemController.updateItem)
+    .delete(itemController.getItemById, auth, itemController.deleteItem);
 
 module.exports = router;
