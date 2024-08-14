052539b9c02372e6bfb07a0f6dcbeba648654c04
KMicheleMoore <kmichele0831@gmail.com>
2024-08-14 16:46:38 -0300

    added report functions

--- app/controllers/reportsController.js
+++ app/controllers/reportsController.js
@@ -1,6 +1,225 @@
 const { models } = require('../data');
 const { calculateCurrentValue } = require('../util/calc');
 
+const getFinancial = async (facility) => {
+
+        const depreciationRate = await models.Setting.findOne({
+            attributes: ['value'],
+            where: { name: 'depreciationRate' }
+        });
+
+        const queryOptions = {
+            attributes: [
+                'id',
+                'name',
+                'vendor',
+                'invoice',
+                'donated',
+                'initialValue',
+                'eol',
+                'createdAt',
+                'updatedAt'
+            ],
+            include: [
+                {
+                    model: models.Unit,
+                    attributes: ['id', 'name'],
+                    include: {
+                        model: models.Facility,
+                        attributes: ['id', 'name']
+                    }
+                },
+                {
+                    model: models.Template,
+                    attributes: ['name']
+                }
+            ]
+        };
+
+        if (facility) {
+            queryOptions.include[0].where = { id: facility }
+        };
+
+        const items = await models.Item.findAll(queryOptions);
+
+        const report = items.map(item => {
+            const currentValue = calculateCurrentValue(item.initialValue, item.createdAt, depreciationRate);
+
+            return {
+                id: item.id,
+                name: item.name,
+                facilityId: item.Unit.Facility.id,
+                facilityName: item.Unit.Facility.name,
+                unitId: item.Unit.id,
+                unitName: item.Unit.name,
+                templateName: item.Template.name,
+                vendor: item.vendor,
+                invoice: item.invoice,
+                donated: item.donated,
+                currentValue: currentValue,
+                eol: item.eol,
+                createdAt: item.createdAt,
+                updatedAt: item.updatedAt
+            };
+        });
+
+        return report;
+}
+
+const getInventory = async (facility) => {
+
+    const depreciationRate = await models.Setting.findOne({
+        attributes: ['value'],
+        where: { name: 'depreciationRate' }
+    });
+
+    const queryOptions = {
+        attributes: [
+            'id',
+            'name',
+            'vendor',
+            'invoice',
+            'status',
+            'donated',
+            'initialValue',
+            'eol',
+            'createdAt',
+            'updatedAt'
+        ],
+        include: [
+            {
+                model: models.Unit,
+                attributes: ['id', 'name'],
+                include: {
+                    model: models.Facility,
+                    attributes: ['id', 'name']
+                }
+            },
+            {
+                model: models.User,
+                as: 'addedByUser',
+                attributes: ['name']
+            },
+            {
+                model: models.Inspection,
+                attributes: [
+                    'userId',
+                    'createdAt',
+                    'comment'
+                ],
+                include: {
+                    model: models.User,
+                    attributes: ['name']
+                },
+                required: false,
+                order: [['createdAt', 'DESC']],
+                limit: 1
+            }
+        ]
+    };
+
+    if (facility) {
+        queryOptions.include[0].where = { id: facility };
+    }
+
+    const items = await models.Item.findAll(queryOptions);
+
+    const report = items.map(item => {
+        const currentValue = calculateCurrentValue(item.initialValue, item.createdAt, depreciationRate);
+        const lastInspection = item.Inspections[0] || {};
+
+        return {
+            id: item.id,
+            name: item.name,
+            facilityId: item.Unit.Facility.id,
+            facilityName: item.Unit.Facility.name,
+            unitId: item.Unit.id,
+            unitName: item.Unit.name,
+            vendor: item.vendor,
+            invoice: item.invoice,
+            addedBy: item.addedByUser.name,
+            status: item.status,
+            inspectedBy: lastInspection.User ? lastInspection.User.name : null,
+            lastInspected: lastInspection.createdAt ? lastInspection.createdAt : null,
+            comment: lastInspection.comment ? lastInspection.comment : null,
+            eol: item.eol,
+            currentValue: currentValue,
+            donated: item.donated,
+            createdAt: item.createdAt,
+            updatedAt: item.updatedAt
+
+        };
+    });
+
+    return report;
+
+};
+
+const getEol = async (facility, startDate, endDate) => {
+
+    const depreciationRate = await models.Setting.findOne({
+        attributes: ['value'],
+        where: { name: 'depreciationRate' }
+    });
+
+    const queryOptions = {
+        attributes: [
+            'id',
+            'name',
+            'vendor',
+            'invoice',
+            'initialValue',
+            'eol',
+            'status',
+            'createdAt',
+            'updatedAt'
+        ],
+        include: [
+            {
+                model: models.Unit,
+                attributes: ['id', 'name'],
+                include: {
+                    model: models.Facility,
+                    attributes: ['id', 'name']
+                }
+            }
+        ]
+    };
+
+    if (facility) {
+        queryOptions.include[0].where = { id: facility };
+    }
+
+    const items = await models.Item.findAll(queryOptions);
+
+    const eolItems = items.filter(item => {
+        const itemEol = new Date(item.eol);
+        return itemEol >= startDate && itemEol <= endDate;
+    });
+
+    const report = eolItems.map(item => {
+        const currentValue = calculateCurrentValue(item.initialValue, item.createdAt, depreciationRate);
+        return {
+            id: item.id,
+            name: item.name,
+            facilityId: item.Unit.Facility.id,
+            facilityName: item.Unit.Facility.name,
+            unitId: item.Unit.id,
+            unitName: item.Unit.name,
+            vendor: item.vendor,
+            invoice: item.invoice,
+            initalValue: item.initialValue,
+            currentValue: currentValue,
+            eol: item.eol,
+            status: item.status,
+            createdAt: item.createdAt,
+            updatedAt: item.updatedAt
+        }
+    });
+
+    return report;
+}
+
 exports.getSummary = async (req, res, next) => {
     try {
         const data = await models.Facility.findAll({
@@ -43,7 +262,7 @@ exports.getSummary = async (req, res, next) => {
                         }, {
                             model: models.Inspection,
                             attributes: [
-                                'id', 
+                                'id',
                                 'comment',
                                 'createdAt'
                             ],
@@ -60,7 +279,7 @@ exports.getSummary = async (req, res, next) => {
 
         const depreciationRate = await models.Setting.findOne({
             attributes: ['value'],
-            where: { name: 'depreciationRate'}
+            where: { name: 'depreciationRate' }
         });
 
         const overview = data.map(facility => {
@@ -145,22 +364,93 @@ exports.getSummary = async (req, res, next) => {
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: 'Server error.'})
+        return res.status(500).json({ error: 'Server error.' })
     }
 };
 
 exports.exportAll = async (req, res, next) => {
+    try {
+        const { facility, startDate, endDate } = req.body;
 
+        if (!startDate || !endDate) {
+            return res.status(400).json({ error: 'Invalid date.' });
+        };
+
+        const eolStart = new Date(startDate);
+        const eolEnd = new Date(endDate);
+
+        if (isNaN(eolStart.getTime()) || isNaN(eolEnd.getTime())) {
+            return res.status(400).json({ error: 'Invalid date.' });
+        };
+
+        const financialReport = await getFinancial(facility);
+        const inventoryReport = await getInventory(facility);
+        const eolReport = await getEol(facility, eolStart, eolEnd);
+
+        const reports = {
+            financial: financialReport,
+            inventory: inventoryReport,
+            eol: eolReport
+        };
+
+        return res.status(200).json(reports);
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error generating report' });
+    }
 };
 
 exports.exportFinancial = async (req, res, next) => {
+    try {
+        const { facility } = req.body;
+        const financialReport = await getFinancial(facility);
 
+        return res.status(200).json(financialReport);
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error generating report' });
+    };
 };
 
 exports.exportInventory = async (req, res, next) => {
+
+    try {
+        const { facility } = req.body;
+        const inventoryReport = await getInventory(facility);
+
+        return res.status(200).json(inventoryReport);
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error generating report' });
+    }
 
 };
 
 exports.exportEOL = async (req, res, next) => {
 
+    try {
+        const { facility, startDate, endDate } = req.body;
+
+        if (!startDate || !endDate) {
+            return res.status(400).json({ error: 'Invalid date.' });
+        };
+
+        const eolStart = new Date(startDate);
+        const eolEnd = new Date(endDate);
+
+        if (isNaN(eolStart.getTime()) || isNaN(eolEnd.getTime())) {
+            return res.status(400).json({ error: 'Invalid date.' });
+        }
+
+        const eolReport = await getEol(facility, eolStart, eolEnd);
+
+        return res.status(200).json(eolReport);
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error generating report' });
+    }
 };
