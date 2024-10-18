55d3779b83534c61605993ad9e637cf36edcf529
tristan-remple <tristan.remple@outlook.com>
2024-10-18 16:08:18 -0300

    icon path?

--- app/routes/sat/icons.js
+++ app/routes/sat/icons.js
@@ -4,9 +4,11 @@ const admin = require('../../middleware/admin');
 const multer = require('multer');
 var _fs = require("fs");
 
+const iconFolder = 'client/public/img'
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'client/public/img');
+        cb(null, iconFolder);
     }
 })
 const upload = multer({ storage: storage });
@@ -15,7 +17,9 @@ const rename = function (req, res, next) {
     var files = req.files;
     if (files) {
         //Move file to the deployment folder.
-        var newPath = `${__dirname}/client/public/img/${req.body.date}-${req.body.name}.${req.body.ext}`;
+        // var newPath = `client/public/img/${req.body.date}-${req.body.name}.${req.body.ext}`;
+        const newPath = `${iconFolder}/${req.body.date}-${req.body.name}.${req.body.ext}`
+        const baseFilename = `${req.body.date}-${req.body.name}.${req.body.ext}`
         console.log(newPath)
         console.log(files[0].path)
         _fs.renameSync(files[0].path, newPath);
