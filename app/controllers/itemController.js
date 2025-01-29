e49f218cd25cf50eab89739765a89b73b790e887
KMicheleMoore <kmichele0831@gmail.com>
2025-01-29 14:59:37 -0400

    eol calc update

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -225,7 +225,7 @@ exports.updateItem = async (req, res, next) => {
             invoice: invoice,
             vendor: vendor,
             initialvalue: initialValue,
-            eol: new Date(eol),
+            eol: eol,
             status: status,
             unit: newUnit
         });
