13ac073dd0eb19baf61a2711597256b73d26b337
kmich <kmichele0831@gmail.com>
2025-04-06 11:26:03 -0300

    updated timestamp in update item

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -216,7 +216,7 @@ exports.updateItem = async (req, res, next) => {
             eol: getEoL(usefulLifeOffset, item.eol),
             status: status,
             unitid: newUnit,
-            createdat: new Date (createdAt)
+            createdat: new Date(createdAt).getTime()
         });
 
         const depreciationRate = await models.setting.findOne({
